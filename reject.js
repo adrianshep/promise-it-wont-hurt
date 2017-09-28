cca37ba8a1722eef758d9d660640934530d5ed48
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-09-28 09:26:28 -0400

    3rd lesson in promise-it-wont-hurt

--- reject.js
+++ reject.js
@@ -1,13 +1,19 @@
+'use strict';
+
 var promise = new Promise(function (fulfill, reject) {
   // Your solution here
+  setTimeout(function () {
+    fulfill('FULFILLED!');
+  }, 300);
 });
 
 function onReject (error) {
   // Your solution here
+reject('REJECTED!');
 }
 
 // Your solution here
-
+promise.then(console.log);
 
 /*
 
