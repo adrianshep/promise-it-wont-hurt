42ebddefc78c6821a3bc8ea9c329820842727c9f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-10-13 09:34:15 -0400

    7th lesson in promise-it-wont-hurt

--- after.js
+++ after.js
@@ -1,4 +1,10 @@
-
+Parse.User.logIn('user', 'pass').then(function (user) {
+  return query.find();
+}).then(function (results) {
+  return results[0].save({ key: value });
+}).then(function (result) {
+  // the object was saved
+});
 
 
 /*
