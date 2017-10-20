0321262102753759cbd27fe8bdeeb21ebf03749e
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-10-20 09:28:29 -0400

    9th lesson in promise-it-wont-hurt

--- throw.js
+++ throw.js
@@ -1,4 +1,6 @@
-
+doSomethingRisky()
+.then(doAnotherRiskyThing)
+.then(null, console.log);
 
 
 /*
@@ -40,8 +42,10 @@ Some invalid JSON will be available on process.argv[2].
 
   * Build a function called `parsePromised` that creates a promise, performs
    `JSON.parse` in a `try`/`catch` block, and fulfills or rejectsthe promise
-    depending on whether an error is thrown.**Note:** your function should
-    synchronously return the promise!
+    depending on whether an error is thrown.
+
+    **Note:** your function should synchronously return the promise!
+
   * Build a sequence of steps like the ones shown above that catchesany thrown
     errors and logs them to the console.
 
