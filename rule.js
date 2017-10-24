63e3232454416d4cd4f2632293a3403d10345ff4
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-10-24 09:24:45 -0400

    10th lesson in promise-it-wont-hurt

--- rule.js
+++ rule.js
@@ -9,6 +9,23 @@ function alwaysThrows () {
   throw new Error('OH NOES');
 }
 
+function onReject(error) {
+  console.log(error.message);
+}
+
+Promise.resolve(iterate(1))
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(alwaysThrows)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.then(iterate)
+.catch(onReject);
+
 
 /*
 
