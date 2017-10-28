0e1e9fb0954bff0044b39b09256ea6ded3258a7d
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-10-28 10:05:59 -0400

    11th lesson in promise-it-wont-hurt

--- multiple.js
+++ multiple.js
@@ -31,6 +31,43 @@ all(getPromise1(), getPromise2())
 
 /*
 
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    'use strict';
+
+    / global getPromise1, getPromise2 /
+
+    function all(a, b) {
+      return new Promise(function (fulfill, reject) {
+        var counter = 0;
+        var out = [];
+
+        a.then(function (val) {
+          out[0] = val;
+          counter++;
+
+          if (counter >= 2) {
+            fulfill(out);
+          }
+        });
+
+        b.then(function (val) {
+          out[1] = val;
+          counter++;
+
+          if (counter >= 2) {
+            fulfill(out);
+          }
+        });
+      });
+    }
+
+    all(getPromise1(), getPromise2())
+      .then(console.log);
+
+────────────────────────────────────────────────────────────────────────────────
+
 Multiple promises
  Exercise 11 of 13
 
