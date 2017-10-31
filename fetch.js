66e330386452766ef7da0d61319e53c8ae27fa74
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-10-31 09:42:13 -0400

    12th lesson in promise-it-wont-hurt

--- fetch.js
+++ fetch.js
@@ -1,5 +1,12 @@
 var qhttp = require('q-io/http');
 
+qhttp.read("http://localhost:1337")
+.then(function (json) {
+  console.log(JSON.parse(json));
+})
+.then(null, console.error)
+.done()
+
 
 /*
 
