6dc9b8a4ea32c2682af968673d485f5a047d4cda
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-04 10:04:29 -0400

    13th lesson in promise-it-wont-hurt

--- work.js
+++ work.js
@@ -1,11 +1,14 @@
 var qhttp = require('q-io/http');
 
-qhttp.read("http://localhost:1337")
+qhttp.read("http://localhost:7000/")
+.then(function (id) {
+  return qhttp.read("http://localhost:7001/" + id);
+})
 .then(function (json) {
   console.log(JSON.parse(json));
 })
 .then(null, console.error)
-.done()
+.done();
 
 
 /*
